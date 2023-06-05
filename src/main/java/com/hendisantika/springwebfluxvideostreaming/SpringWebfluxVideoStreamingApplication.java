6c2fe0c5aaf9350931db49167444923abd86463d
hendisantika <hendisantika@yahoo.co.id>
2023-06-05 17:59:47 +0700

    #6 - Add CommandLineRunner ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springwebfluxvideostreaming/SpringWebfluxVideoStreamingApplication.java
+++ src/main/java/com/hendisantika/springwebfluxvideostreaming/SpringWebfluxVideoStreamingApplication.java
@@ -1,13 +1,20 @@
 package com.hendisantika.springwebfluxvideostreaming;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 
 @SpringBootApplication
-public class SpringWebfluxVideoStreamingApplication {
+public class SpringWebfluxVideoStreamingApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebfluxVideoStreamingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        DefaultDataBufferFactory sharedInstance = DefaultDataBufferFactory.sharedInstance;
     }
 
 }
