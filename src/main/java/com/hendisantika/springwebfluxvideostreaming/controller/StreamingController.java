d94411ea5c04514f733baf146afe2814f335a481
hendisantika <hendisantika@yahoo.co.id>
2023-06-05 18:01:04 +0700

    #8 - Add getVideo in StreamingController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springwebfluxvideostreaming/controller/StreamingController.java
+++ src/main/java/com/hendisantika/springwebfluxvideostreaming/controller/StreamingController.java
@@ -2,7 +2,13 @@ package com.hendisantika.springwebfluxvideostreaming.controller;
 
 import com.hendisantika.springwebfluxvideostreaming.service.StreamingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,7 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class StreamingController {
 
     private final StreamingService streamingService;
+
+    @GetMapping(value = "video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideo(@PathVariable String title, @RequestHeader("Range") String range) {
+        log.info(range);
+        return streamingService.getVideo(title);
+    }
 }
