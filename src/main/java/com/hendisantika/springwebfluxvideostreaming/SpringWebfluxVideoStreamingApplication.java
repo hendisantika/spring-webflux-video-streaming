7c1fae789fe3fc2e50b93ad53a53fc37a32f5d3d
hendisantika <hendisantika@yahoo.co.id>
2023-09-19 05:22:32 +0700

    #16 - Bug Fix error ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springwebfluxvideostreaming/SpringWebfluxVideoStreamingApplication.java
+++ src/main/java/com/hendisantika/springwebfluxvideostreaming/SpringWebfluxVideoStreamingApplication.java
@@ -1,22 +1,13 @@
 package com.hendisantika.springwebfluxvideostreaming;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
-public class SpringWebfluxVideoStreamingApplication implements CommandLineRunner {
+public class SpringWebfluxVideoStreamingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebfluxVideoStreamingApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        DefaultDataBufferFactory sharedInstance = DefaultDataBufferFactory.sharedInstance;
     }
 
 }
